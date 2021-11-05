241e21b71cdb4463557e056f5e69b262f04f6b99
phuockaus <phuoc.doan0412@hcmut.edu.vn>
2021-11-05 11:30:21 +0700

    Add module Input.py

--- Reader.py
+++ Reader.py
@@ -1,12 +1,4 @@
 from abc import ABC
-from Reader.DOCXReader import DOCXReader
-from Reader.PDFReader import PDFReader
-
-
-def readInputs(folder):
-    pdf_data = PDFReader.getPDFList(folder)
-    docx_data = DOCXReader.getDOCXList(folder)
-    return pdf_data + docx_data
 
 
 class FileObj(ABC):
